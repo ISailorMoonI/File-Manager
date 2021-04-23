28a1ca2e9c90c621bba46e8d51ce4d0c13bb6e6c
SailorMoon <yashamaru99@gmail.com>
2021-04-24 02:37:46 +0300

    Added catch Exceptions in ShowInfo Class

--- File Manager/ShowInfo.cs
+++ File Manager/ShowInfo.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using System.Collections;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 
 namespace File_Manager
 {
@@ -13,70 +15,123 @@ namespace File_Manager
 
         public string CurrentDir()
         {
-            Console.WriteLine("Enter path");
-            this.DirInfo = Console.ReadLine();
-            Console.WriteLine("Current directory  is: {0} \n", DirInfo);
-            return DirInfo;
+            try
+            {
+                Console.WriteLine("Enter path");
+                this.DirInfo = Console.ReadLine();
+                Console.WriteLine("Current directory  is: {0} \n", DirInfo);
+                return DirInfo;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.Message);
+                throw;
+            }
+
         }
 
         //Выводит список папок в директории в сокращенной записи
         public void ShowDir()
         {
-            //Console.WriteLine("Enter path");
-            //string pathString = Console.ReadLine();
-            Console.WriteLine("====================================================================");
-            Console.WriteLine("The directory {0} contains the following directories: \n", this.DirInfo);
-            foreach (var folder in Directory.GetDirectories(this.DirInfo))
+            try
             {
-                Console.WriteLine(Path.GetFileName(folder));
+                Console.WriteLine("====================================================================");
+                Console.WriteLine("The directory {0} contains the following directories: \n", this.DirInfo);
+                foreach (var folder in Directory.GetDirectories(this.DirInfo))
+                {
+                    Console.WriteLine(Path.GetFileName(folder));
+                }
+                Console.WriteLine("====================================================================");
             }
-            Console.WriteLine("====================================================================");
+            catch(Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.Message);
+                return;
+            }
         }
         //Выводит список файлов в ПАПКЕ и их размер без указания пути
         public void ShowFiles()
         {
-
-            DirectoryInfo directory = new DirectoryInfo(DirInfo);
-            FileInfo[] fileArray = directory.GetFiles();
-            Console.WriteLine("====================================================================");
-            Console.WriteLine("The directory {0} contains the following files: \n", directory.Name);
-            foreach (FileInfo file in fileArray)
+            try
             {
-                Console.WriteLine($"{file.Name}, | {file.Length} bytes");
+                DirectoryInfo directory = new DirectoryInfo(DirInfo);
+                FileInfo[] fileArray = directory.GetFiles();
+                Console.WriteLine("====================================================================");
+                Console.WriteLine("The directory {0} contains the following files: \n", directory.Name);
+                foreach (FileInfo file in fileArray)
+                {
+                    Console.WriteLine($"{file.Name}, | {file.Length} bytes");
+                }
+                Console.WriteLine("====================================================================");
             }
-            Console.WriteLine("====================================================================");
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.Message);
+                return;
+            }
+            
         }
 
         //Выводит список папок с полным путем
-        public void ShowDirectory()
-        {
-            string[] directoryArray = Directory.GetDirectories(DirInfo);
-            foreach (string directory in directoryArray)
+        
+            public void ShowDirectory()
             {
-                Console.WriteLine($"{directory}");
-            }
+                try
+                {
+                    string[] directoryArray = Directory.GetDirectories(DirInfo);
+                    foreach (string directory in directoryArray)
+                    {
+                        Console.WriteLine($"{directory}");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("The process failed: {0}", e.Message);
+                    return;
+                }
         }
+        
+        
+
         //Выводит общий размер папки
         public long GetDirectorySize()
         {
-            string[] a = Directory.GetFiles(DirInfo, "*.*");
-            long size = 0;
-            foreach (string name in a)
+            try
             {
-                FileInfo info = new FileInfo(name);
-                size += info.Length;
+                string[] a = Directory.GetFiles(DirInfo, "*.*");
+                long size = 0;
+                foreach (string name in a)
+                {
+                    FileInfo info = new FileInfo(name);
+                    size += info.Length;
+                }
+
+                Console.WriteLine("The Directory {0} size is {1} bytes ", DirInfo, size);
+                return size;
             }
-            Console.WriteLine("The Directory {0} size is {1} bytes ", DirInfo, size);
-            return size;
-            
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.Message);
+                throw;
+            }
+
         }
         public void DirSize()
         {
-            string[] directoryArray = Directory.GetDirectories(DirInfo);
-            foreach (string directory in directoryArray)
+            try
             {
-                Console.WriteLine($"{directory}, {directory.Length} bytes");
+                string[] directoryArray = Directory.GetDirectories(DirInfo);
+                foreach (string directory in directoryArray)
+                {
+                    Console.WriteLine($"{directory}, {directory.Length} bytes");
+                }
             }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.Message);
+                return;
+            }
+
         }
     }
 }
